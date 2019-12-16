eee3dd026832b3047e718d9e975f17433067989b
SolovievaAnastasia <55579608+SolovievaAnastasia@users.noreply.github.com>
2019-12-16 21:23:41 +0200

    Update L8-Функции и файлы.cpp

--- L8/L8-Функции и файлы.cpp
+++ L8/L8-Функции и файлы.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include math.h
 using namespace std;
 
 struct NOTEBOOK {
@@ -32,33 +33,36 @@ for (i=0; i < num; i++)
     myfile.close();
 }
 
-void write(NOTEBOOK zap[] ,int num) {
- file.open ("bin.txt", out | binary);
-
- file.write((char*)&num, sizeof(num));      
-   file.seekp(0, end);
-	for (i=0; i < num; i++)
-	{		 
+void write(NOTEBOOK zap[] ,int i) {
+	 
 file.write((char*)&zap[i].model,sizeof(  zap[i].model));
 file.write((char*)&zap[i].num[i].x,sizeof(zap[i].num[i].x));
 file.write((char*)&zap[i].num[i].y,sizeof(zap[i].num[i].y));
 file.write((char*)&zap[i].num[i].z,sizeof(zap[i].num[i].z));
 file.write((char*)&zap[i].w,sizeof(zap[i].w));
 file.write((char*)&zap[i].price,sizeof(zap[i].price));
-	}
-	file.close();
+
 }
 
-
+	
+}
 int main()
 {
 setlocale(0,"");
 NOTEBOOK zap[21];
 size num[21];
-
+double diag;
 read(NOTEBOOK zap[21], 21);
-write(NOTEBOOK zap[21], 21);	
-wrdiag(	NOTEBOOK zap[21], 21);
 	
+ file.open ("bin.txt", out | binary);
+ file.write((char*)&num, sizeof(num));      
+ file.seekp(0, end);
+	for (i=0; i < num; i++)
+	diag = sqrt(pow(zap[i].num[i].x, 2)+pow(zap[i].num[i].y, 2));
+	if (diag > 11){
+		write(NOTEBOOK zap[21], i);	
+	}
+file.close();
+
 return 0;
 }
